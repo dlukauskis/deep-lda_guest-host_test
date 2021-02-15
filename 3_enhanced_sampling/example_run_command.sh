944faf0508b04e453eb6ea6c9811f92d43688401
dlukauskis <50554130+dlukauskis@users.noreply.github.com>
2021-02-15 13:03:48 +0000

    Add files via upload

---
+++ 3_enhanced_sampling/example_run_command.sh
@@ -0,0 +1,1 @@
+/home/dom/Software/gromacs-2019.6-plumed-2.7.0-thread-MPI/install/bin/gmx mdrun -s topol.tpr -deffnm prod -plumed plumed.dat -pin on -pinoffset 4 -pinstride 1 -nb cpu -pme cpu -bonded cpu -v
---
+++ 3_enhanced_sampling/setup.sh
@@ -0,0 +1,1 @@
+gmx grompp -f 100ns_prod.mdp -c system.gro -p system.top -o 0_run/topol.tpr
